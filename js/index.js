fbcf808fc7eebe7c3d374636325792e05e872e6c
Jonathan Alexander <notdisliked@gmail.com>
2017-05-08 10:00:19 -0700

    Add tab animation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,47 @@
-var linkGrowAnim = anime({
-    targets: link,
-    fontSize: '24px'
-});
-
-var linkAnimateGrow = function(link) {
-    linkGrowAnim.play();
-};
-
-var linkAnimateShrink = function(link) {
-    linkGrowAnim.reverse();
+window.onload = function() {
+    var title = document.getElementById('mainBarTitle');
+    var projects = document.getElementById("mainBarProjects");
+    var about = document.getElementById("mainBarAbout");
+    title.onmouseenter = function() {
+        anime.remove("#mainBarTitle");
+        anime({
+            targets: "#mainBarTitle",
+            fontSize: '18px'
+        });
+    };
+    title.onmouseout = function() {
+        anime.remove("#mainBarTitle");
+        anime({
+            targets: "#mainBarTitle",
+            fontSize: '16px'
+        });
+    };
+    projects.onmouseenter = function() {
+        anime.remove("#mainBarProjects");
+        anime({
+            targets: "#mainBarProjects",
+            fontSize: '18px'
+        });
+    };
+    projects.onmouseout = function() {
+        anime.remove("#mainBarProjects");
+        anime({
+            targets: "#mainBarProjects",
+            fontSize: '16px'
+        });
+    };
+    about.onmouseenter = function() {
+        anime.remove("#mainBarAbout");
+        anime({
+            targets: "#mainBarAbout",
+            fontSize: '18px'
+        });
+    };
+    about.onmouseout = function() {
+        anime.remove("#mainBarAbout");
+        anime({
+            targets: "#mainBarAbout",
+            fontSize: '16px'
+        });
+    };
 };
