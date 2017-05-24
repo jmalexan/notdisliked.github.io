ae0c7ecbe7e0a33c2f85d39adfef5c456aca9b55
Jonathan Alexander <me@jmalexan.com>
2017-05-24 09:00:29 -0700

    WIP tab typing animation and iframe rework

--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
 window.onload = function() {
+    var kps = 15;
+    var mspk = (1 / kps) * 1000;
     var title = document.getElementById('mainBarTitle');
     var projects = document.getElementById("mainBarProjects");
     var about = document.getElementById("mainBarAbout");
+    var titleFinalText = title.innerHTML
+    var projectsFinalText = projects.innerHTML
+    var aboutFinalText = about.innerHTML
+    title.innerHTML = ">_"
+    projects.innerHTML = ">_"
+    about.innerHTML = ">_"
+    anime({
+        duration: titleFinalText.length * mspk,
+        easing: 'linear',
+        update: function(anim) {
+            title.innerHTML = ">" + titleFinalText.substr(0, Math.floor(titleFinalText.length * (anim.progress / 100))) + "_"
+        },
+        complete: function(anim) {
+            anime({
+                duration: projectsFinalText.length * mspk,
+                easing: 'linear',
+                update: function(anim) {
+                    projects.innerHTML = ">" + projectsFinalText.substr(0, Math.floor(projectsFinalText.length * (anim.progress / 100))) + "_"
+                },
+                complete: function(anim) {
+                    anime({
+                        duration: aboutFinalText.length * mspk,
+                        easing: 'linear',
+                        update: function(anim) {
+                            about.innerHTML = ">" + aboutFinalText.substr(0, Math.floor(aboutFinalText.length * (anim.progress / 100))) + "_"
+                        }
+                    });
+                }
+            });
+        }
+    });
     title.onmouseenter = function() {
         anime.remove("#mainBarTitle");
         anime({
